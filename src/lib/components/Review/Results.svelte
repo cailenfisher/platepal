a6a40b1badd1f07ee8ae48c1a896220cf1f51a8e
Cailen Fisher <cailen.fisher@gmail.com>
2023-01-15 21:15:18 -0500

    display review comments

--- src/lib/components/Review/Results.svelte
+++ src/lib/components/Review/Results.svelte
@@ -5,6 +5,7 @@
 
     let reviewCount: Number | undefined = 0;
     let reviewAverage: Number = 0.0;
+    let reviews: Array<object> | null
 
     let loadReviews = async () => {
         const { data, error } = await supabase
@@ -12,9 +13,13 @@
         .select()
         .eq('meal', meal);
 
-        reviewCount = data?.length;
-        reviewAverage = data.reduce((total, next) => total + next.rating, 0) / data.length;
-        console.log('results review', data)
+        if (data) {
+            reviews = data;
+            reviewCount = data?.length;
+            reviewAverage = data.reduce((total, next) => total + next.rating, 0) / data.length;
+            console.log('results review', data)
+        }
+        
     }
 
     onMount(async () => {
@@ -37,4 +42,16 @@
 
 <h1>Reviews: {reviewCount}</h1>
 <h2>Average Rating: {reviewAverage}</h2>
+
+{#if reviews}
+<div class="pt-10 space-y-2">
+    {#each reviews as review}
+    <div class="card text-warning-500">
+        <!-- <div class="text-xl">{review.created_at}</div> -->
+        <div class="text-primary-50">{review.rating} Stars</div>
+        <div class="">{review.comment}</div>
+    </div>
+    {/each}
+</div>
+{/if}
 
